47131d68208e915da727279e459f83e05efa2ebc
ramaobulreddy <ramaobulreddy.e@gmail.com>
2026-01-21 10:53:08 +0000

    python variables

---
+++ statements.py
@@ -0,0 +1,11 @@
+print("Python is fun!")
+
+#Statements in different lines 
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+#Statements in a single line  
+
+print("Hello"); print("How are you?"); print("Bye bye!")
