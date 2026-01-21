c4a671a8df39d84c7e180c8ae6f4eb9004e64b05
ramaobulreddy <ramaobulreddy.e@gmail.com>
2026-01-21 09:34:27 +0000

    indentation testing

--- indentation.py
+++ indentation.py
@@ -1,2 +1,8 @@
 if 5 > 2:
-  print("Five is greater than two!")
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+#Python will give you an error if you skip the indentation , Indentation refers to the spaces at the beginning of a code line.
+if 5 > 2:
+ print("Five is greater than two!") 
