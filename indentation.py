599ab56b66e076330f61a10278e5165ad349b95d
ramaobulreddy <ramaobulreddy.e@gmail.com>
2026-01-23 16:07:57 +0000

    Pipeline

--- indentation.py
+++
@@ -1,8 +0,0 @@
-if 5 > 2:
- print("Five is greater than two!") 
-if 5 > 2:
-        print("Five is greater than two!")
-
-#Python will give you an error if you skip the indentation , Indentation refers to the spaces at the beginning of a code line.
-if 5 > 2:
- print("Five is greater than two!") 
--- printtext.py
+++
@@ -1,3 +0,0 @@
-#print in a sigle line
-print("Hello World!", end=" ")
-print("I will print on the same line.")
